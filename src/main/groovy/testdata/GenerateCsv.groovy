cd112e54c7066baf2cdf30ff7c08ce5da6ac3830
Rahul Somasunderam <rsom@certifydatasystems.com>
2013-08-09 14:32:18 -0700

    Added capability to generate csv files

---
+++ src/main/groovy/testdata/GenerateCsv.groovy
@@ -0,0 +1,46 @@
+package testdata
+
+import domain.Person
+
+import java.text.DecimalFormat
+
+/**
+ * Generates a CSV with patient data for specified number of patients
+ * @author rahulsomasunderam
+ */
+class GenerateCsv {
+  private static final clinicAddress = '560 S Winchester Blvd, San Jose CA 95128'
+  private static patients = 1000
+
+  public static void main(String[] args) {
+
+    def util = new PersonFactory(maxAddresses: patients / 2000)
+    util.setCenter(clinicAddress)
+
+    String nsid = "1005.1"
+    String oid = "2.16.4.39.2.1001.78.5.1"
+
+    def f = new File('/tmp/pat.csv').newPrintWriter()
+    f.println (['firstname','lastName','dob','gender','street','city','state', 'zipCode','country',
+        'phone','ssn','id','nsid','uid','uidtype'].collect {"\"${it}\""}.join(','))
+
+    patients.times { index ->
+      Person p = util.generatePerson()
+      def a = p.address
+
+      def cols = [p.firstName,p.lastName,p.dob.format('yyyyMMdd'),p.gender,a.street,a.city,a.state, a.zipCode,"USA",
+          p.phone,p.ssn,'B' + new DecimalFormat('00000000').format(index + 10000000),nsid,oid,'ISO']
+
+      f.println cols.collect {"\"${it}\""}.join(',')
+
+      if (index % 10000 == 0) {
+        println index
+      }
+    }
+
+    f.flush()
+    f.close()
+
+  }
+
+}
